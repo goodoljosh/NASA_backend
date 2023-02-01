8f6ab24d6a02821c75429e483e19b16ddb7e49b1
goodoljosh <findley_joshua1@columbusstate.edu>
2023-02-01 00:00:26 +0000

    Added package.json, express and axios downloads.

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var axios = require('axios');
+
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
+
+app.listen(3000,function(){
+  console.log('App listening on port 3000');
+
+})
