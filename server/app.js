c4018deb2097952503289b0ed7c48ea0b201754b
goodoljosh <findley_joshua1@columbusstate.edu>
2023-02-07 23:26:17 +0000

    Added api get function.

--- server/app.js
+++ server/app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 var axios = require('axios');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.get("/api", (req, res) => {
-res.json({message: "NASA"});
+app.get("/api", function(req, res)  {
+  axios.get('https://api.nasa.gov/planetary/apod'+ '&apikey=mPR4K9KTmxTBtepFRlKkmGHaVyKHvTuo6NFsiKdp' ).then(function(response){
+    res.render('apihtml.ejs', {data : response.data });
+  })
 });
 
 app.listen(PORT,() => {
   console.log(`Server listening on ${PORT}`);
-
 });
-
-//Nasa API Key: mPR4K9KTmxTBtepFRlKkmGHaVyKHvTuo6NFsiKdp 
