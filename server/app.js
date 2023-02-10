a9823edc870503e86af89e43aca19be057a4cd87
goodoljosh <findley_joshua1@columbusstate.edu>
2023-02-10 00:22:42 +0000

    Returned JSON objects.

--- server/app.js
+++ server/app.js
@@ -3,9 +3,15 @@ const app = express();
 var axios = require('axios');
 const PORT = process.env.PORT || 3001;
 
+// app.get("/", function(req, res)  {
+//   axios.get('https://api.nasa.gov/planetary/apod?'+ 'api_key=mPR4K9KTmxTBtepFRlKkmGHaVyKHvTuo6NFsiKdp' ).then(function(response){
+//     res.render('apihtml.ejs', {data : response.data });
+//   })
+// });
+
 app.get("/", function(req, res)  {
   axios.get('https://api.nasa.gov/planetary/apod?'+ 'api_key=mPR4K9KTmxTBtepFRlKkmGHaVyKHvTuo6NFsiKdp' ).then(function(response){
-    res.render('apihtml.ejs', {data : response.data });
+   res.send(JSON.stringify(response.data));
   })
 });
 
