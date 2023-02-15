24ee9d3f70386becba95d6fae9450dfe32bfcfb1
goodoljosh <findley_joshua1@columbusstate.edu>
2023-02-15 00:07:04 +0000

    Pushed backend to render.

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const app = express();
 var axios = require('axios');
 const PORT = process.env.PORT || 3001;
 
+//https://group-project.onrender.com
+
 // app.get("/", function(req, res)  {
 //   axios.get('https://api.nasa.gov/planetary/apod?'+ 'api_key=mPR4K9KTmxTBtepFRlKkmGHaVyKHvTuo6NFsiKdp' ).then(function(response){
 //     res.render('apihtml.ejs', {data : response.data });
