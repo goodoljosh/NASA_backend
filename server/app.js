658dbf53501a215fea7d7e306bd5722c90169017
goodoljosh <findley_joshua1@columbusstate.edu>
2023-02-17 00:04:01 +0000

    Added backend infrastructure

--- server/app.js
+++ server/app.js
@@ -3,6 +3,17 @@ const app = express();
 var axios = require('axios');
 const PORT = process.env.PORT || 3001;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Expose-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "https://3000-csuwebdev-nasafrontend-hdgdlct877p.ws-us87.gitpod.io/");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+})
+
 //https://group-project.onrender.com
 
 // app.get("/", function(req, res)  {
@@ -17,6 +28,8 @@ app.get("/", function(req, res)  {
   })
 });
 
+
 app.listen(PORT,() => {
   console.log(`Server listening on ${PORT}`);
 });
+
