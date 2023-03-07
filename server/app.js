835f901f63543dd6d1bf51becc4fc7856515d1aa
goodoljosh <findley_joshua1@columbusstate.edu>
2023-03-07 23:33:08 +0000

    Added destinations for api calls.

--- server/app.js
+++ server/app.js
@@ -22,13 +22,13 @@ const options = {
 //   })
 // });
 //Picture of the day API call
-app.get("/", function(req, res)  {
+app.get("/npod", function(req, res)  {
   axios.get('https://api.nasa.gov/planetary/apod?'+ 'api_key=mPR4K9KTmxTBtepFRlKkmGHaVyKHvTuo6NFsiKdp' ).then(function(response){
     res.status(200).json(JSON.stringify(response.data))
   })
 });
 //Mars Rover API call
-app.get("/", function(req, res)  {
+app.get("/mars", function(req, res)  {
   axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&'+ 'api_key=mPR4K9KTmxTBtepFRlKkmGHaVyKHvTuo6NFsiKdp' ).then(function(response){
     res.status(200).json(JSON.stringify(response.data))
   })
